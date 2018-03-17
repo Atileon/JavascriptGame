d974f6b2fb4a41608762c20b446a9c727018a374
Steve N. Peralta R.(Atileon) <atileon.v.a@gmail.com>
2018-03-18 00:54:18 +0100

    now the players can obtain the items with the keycode 72 (H)

--- js/avaGame.js
+++ js/avaGame.js
@@ -12,7 +12,7 @@
 //    -create the player Obj
 //    -create weapons on map
 //    -update all functions with current values
-
+'use strict';
 //=========================CLASSES==============================
 class Map {
     constructor(cols,rows,tileW,tileH,arr,ctx){
@@ -33,15 +33,13 @@ class Component{
         this.area = area;
         //next 4 lines to set a random position for the players
         // let tileId = Math.floor(Math.random()*100-1);//This because we are assuming visualy that there are 100 cells but on array are just 99
-        this.tileId = this.startArea();// this value to compare tile ids of any component
+        this.tileId = this.startArea();// this simply to set an tile to deploy component followin the starArea method
         this.x = Math.floor(this.tileId % 10)*64;//value on X axis
         this.y = Math.floor(this.tileId / 10)*64;//value on Y axis
         
         this.w = width;
         this.h = height;
         this.color = color;
-        this.moveX = 0;
-        this.moveY = 0;
     }
     startArea(){
         switch(this.area){
@@ -77,18 +75,22 @@ class Weapon extends Component {
 }
 
 class Player extends Component {
-    constructor(area,x,y,width,height,color,name,weaponObj){
+    constructor(area,x,y,width,height,color,name,weapons){
         super(area,x,y,width,height,color);
         
 
         this.name = name;
         this.health = 100;
-        this.weapon = weaponObj;
-        this.damage = weaponObj.damage;
+        this.weaponArr = weapons;
+        this.weapon = this.weaponArr[0];
         this.posX = this.x;//initial position on X
         this.posY = this.y;//initial position on Y
+        // this.upgradeWeapon = this.weaponNew();
     }
+    upWeapon(i){
+         this.nw = this.weaponArr[i];
 
+    }
     newPos(){
         this.x = this.posX;//position updated on X
         this.y = this.posY;//position updated on Y
@@ -128,7 +130,7 @@ class Player extends Component {
 }
 
 
-//========================================================
+//========================THE GAME================================
 window.addEventListener("load", WindowLoaded, false);
 
 //this Debugger would catch with a function/behavior 
@@ -149,13 +151,7 @@ function WindowLoaded (){
     
 }
 
-function avaGame(){
-
-    let now;
-    let dt = 0;
-    let lastT = performance.now();
-
-    
+function avaGame(){    
     //This is the main function and here would be the global variables
     let canvas = document.getElementById('canvas');
     let context = canvas.getContext('2d');
@@ -172,28 +168,27 @@ function avaGame(){
         [21,22,21,21,22,31,22,21,31,22],
         [21,36,23,23,37,21,36,23,23,37]
     ];
-    //time Settings
-    let startTime = -1;
-    let transitionLength = 500;// milliseconds
-
 
 
     // lets crete the game area
     let gameArea = new Map(10,10,64,64,map,context);
 
-    let weapons = [];
+    let weapons = [];//into this array weapon[0] is the basic weapon for the base player damage
     let players = [];
 
-    let tileSheet = new Image();
+    let tileSheet = new Image();//Actually this is the tilesheet image for the map
     tileSheet.addEventListener('load', drawGame, false);
     tileSheet.src = '../img/tilesheet.png'
+
+
+
     //Now we must create some weapons to push on the weapons[] empty array
-    let claw = new Weapon('b',0,0,64,64,'brown','claw',35);
-    weapons.push(claw);
+    let tomatoe = new Weapon('b',0,0,64,64,'brown','tomatoe',10);
+    weapons.push(tomatoe);
     let banana = new Weapon('b',0,0,64,64,'yellow','banana',50);
     weapons.push(banana);
-    let whipe = new Weapon('b',0,0,64,64,'red','whipe',60);
-    weapons.push(whipe);
+    let pera = new Weapon('b',0,0,64,64,'red','pera',60);
+    weapons.push(pera);
     let papaya = new Weapon('b',0,0,64,64,'orange','papaya',60);
     weapons.push(papaya);
 
@@ -202,9 +197,9 @@ function avaGame(){
 
     //As the weapons Objects we could push the players into an empty array
     // in this case the players[] array
-    let carlitos = new Player('a',0,0,64,64,'black','carlitos',weapons[0]);
+    let carlitos = new Player('a',0,0,64,64,'black','carlitos',weapons);
     players.push(carlitos);
-    let lucifera = new Player('c',0,0,64,64,'white','Lucifera',weapons[0]);
+    let lucifera = new Player('c',0,0,64,64,'white','Lucifera',weapons);
     players.push(lucifera);
 
     Debugger.log('players are on the arena: ');
@@ -216,7 +211,10 @@ function avaGame(){
     console.log('the id of player 1 is: '+p1.tileId);
     console.log('the id of player 2 is: '+p2.tileId);
 
-    console.log('the id of '+ weapons[0].name+' is: '+weapons[0].tileId);
+    console.log('the tile id of weapon  '+p1.weaponArr[1].tileId);
+
+
+    console.log('the id of '+ weapons[0].x+' is: '+weapons[0].tileId);
     console.log('the id of '+ weapons[1].name+' is: '+weapons[1].tileId);
     console.log('the id of '+ weapons[2].name+' is: '+weapons[2].tileId);
     console.log('the id of '+ weapons[3].name+' is: '+weapons[3].tileId);
@@ -248,42 +246,54 @@ function avaGame(){
     //turn mechanism TODO
     let moveCounter = 0;
     let playerId = 0;
+    let turnChange = false;
+
     window.addEventListener('keydown', function(e){movement(players[playerId],e)});
     
     function movement(obj,e){
 
         switch(e.keyCode){
-            case 38://up arrow
+            case 87://up arrow (W)
             obj.moveUp();
             moveCounter +=1;
             break;
-            case 40://down arrow
+
+            case 83://down arrow (S)
             obj.moveDown();
             moveCounter +=1;
             break;
-            case 37://left arrow
+
+            case 65://left arrow (A)
             obj.moveLeft();
             moveCounter +=1;
             break;
-            case 39://right arrow
+
+            case 68://right arrow (D)
             obj.moveRight();
             moveCounter +=1;
             break;
+
             case 13:// Enter keyboard, this to switch immediatly to another player
-            moveCounter +=3;
+            moveCounter +=4;
             break;
+
+            case 72: // Key (H) to take weapon
+            getWeapon(players,weapons);
+            break;
+
         }
         console.log('here the player is: '+playerId);
         if(moveCounter >= 3){
             playerId ++;
-            
+
             console.log('the player on Array is: '+playerId);
+            console.log(players);
 
             if (playerId == players.length){
                 
                 playerId = 0;
             }
-            console.log('switch to player '+ playerId);
+            console.log('switch to player '+ (playerId +1));
             console.log('the movecounter now: '+moveCounter);
             moveCounter = 0;
             console.log('the movecounter then: '+moveCounter);
@@ -292,6 +302,20 @@ function avaGame(){
         return playerId;
     }
     
+    function getWeapon(playerArr,weapArray){
+
+        for(let p = 0; p < playerArr.length; p++){
+            for(let i=0; i < weapArray.length; i++){
+                if( playerArr[p].x == weapArray[i].x && playerArr[p].y == weapArray[i].y){
+                    playerArr[p].weapon = weapArray[i];
+                }else{
+                    playerArr[p].weapon;
+                }           
+             }
+             console.log(playerArr[p].name+' did take the '+playerArr[p].weapon.name);
+        }
+        
+    }
 
   
     //==================================================
@@ -304,42 +328,36 @@ function avaGame(){
             weapons[3].drawIt(context);
         }
         function drawPlayers(){
+            
             p1.drawIt(context);
             p2.drawIt(context);
-        }
-        function preventNextPlayers(){
-            if(p1.tileId >= 52 && p2.tileId <= 47){
-                drawPlayers();
-            }else{
-                updateGae();
-            }
+            p1.newPos();
+            p2.newPos();
+            // getWeapon(players,weapons);
+            
         }
         
-
        drawMap();
        drawComponents();
-    //    preventNextPlayers();
+
        drawPlayers();
-           
+       
+
        
 
     }
 
     function updateGame(){
-        
-        clearCanvas();//this clear the canvas
-        p1.newPos();
-        p2.newPos();//this take the new position after mouse input on canvas for player 1
-        drawGame();//this draw the canvas again with positions updated
         requestAnimationFrame(updateGame);
+
+        clearCanvas();//this clear the canvas
+        
+        drawGame();//this draw the canvas again with positions updated
     }
     //============================
     //keyboard Controll
-    
     // movement(p2);
     
-    
-
     drawGame();
     requestAnimationFrame(updateGame);
     
